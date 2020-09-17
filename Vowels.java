997692eae0fd07c223f42b290e2ee851b5c6ad00
Kalpan Mehta <kalpan.mehta@gmail.com>
2020-09-16 20:26:12 -0400

    Fixed some corner cases

--- Vowels.java
+++ Vowels.java
@@ -1,17 +1,25 @@
 package com.flexon.javabasics;
 
+import java.util.Scanner;
+
 public class Vowels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "ABCDEFDEF";
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the input here: ");
+		String s = sc.nextLine();
+		if (s==null || s.isEmpty()) {
+			System.out.println("Run this code again and input a string.");
+		}
+		else {
 		Integer count = 0;
-		String strlowercase = str.toLowerCase();
-		for (int i = 0; i <str.length(); i++) {
+		String strlowercase = s.toLowerCase();
+		for (int i = 0; i <s.length(); i++) {
 			if (strlowercase.charAt(i) == 'a' || strlowercase.charAt(i) == 'e' ||strlowercase.charAt(i) == 'i' || strlowercase.charAt(i) == 'o' || strlowercase.charAt(i) == 'u' )
 				count  = count + 1 ; 
 		}
-		System.out.println(count);
+		System.out.println("Total number of vowels in this input string are: "+ count);
+		}
 	}
 
 }
